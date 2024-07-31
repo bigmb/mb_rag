689e808501895df4d91077c19bde6cb83cccc0f4
Malav <malav.b93@gmail.com>
2024-07-31 22:01:45 +0100

    updated version.py to 1.0.2

---
+++ mb_rag/chatbot/basic.py
@@ -0,0 +1,47 @@
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+import os
+
+def load_env(file_path: str):
+    """
+    Load environment variables from a file
+    Args:
+        file_path (str): Path to the environment file
+    Returns:
+        None
+    """
+    load_dotenv(file_path)
+    
+def add_os_key(name: str ,key: str):
+    """
+    Add an API key to the environment
+    Args:
+        name (str): Name of the API key
+        key (str): API key
+    Returns:
+        None
+    """
+    os.environ[name] = key
+
+def get_chatbot_openai(model_name: str = "gpt-4o",**kwargs):
+    """
+    Load the chatbot model from OpenAI
+    Args:
+        model_name (str): Name of the model
+        **kwargs: Additional arguments (temperature, max_tokens, timeout, max_retries, apt_key etc.)
+    Returns:
+        ChatOpenAI: Chatbot model
+    """
+    return ChatOpenAI(model_name, **kwargs)
+
+def ask_question(chatbot, question: str):
+    """
+    Ask a question to the chatbot
+    Args:
+        chatbot (ChatOpenAI): Chatbot model
+        question (str): Question to ask
+    Returns:
+        str: Answer from the chatbot
+    """
+    return chatbot.ask(question)
+
