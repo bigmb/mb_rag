072098b5e9696034a25d0ea31fd92d799c82cdf7
Malav <malav.b93@gmail.com>
2024-08-16 00:06:36 +0100

    updated version.py to 1.0.16

--- mb_rag/rag/embeddings.py
+++ mb_rag/rag/embeddings.py
@@ -16,13 +16,134 @@ class embedding_generator:
     Class to generate embeddings for the RAG model
     """
 
-    def __init__(self) -> None:
-        pass
+    def __init__(self,model: str = 'openai',model_type: str = 'text-embedding-3-small',vector_store_type:str = 'chroma' ,logger= None,**kwargs) -> None:
+        self.model = self.load_model(model,model_type,**kwargs)
+        self.vector_store = self.load_vectorstore(vector_store_type,**kwargs)
+        self.logger = logger
 
-    def generate_embeddings():
-        pass
+    def check_file(self, file_path):
+        """
+        Check if the file exists
+        """
+        if os.path.exists(file_path):
+            return True
+        else:
+            return False
 
-    def load_embeddings():
-        pass
+    def generate_text_embeddings(self,text_data_path: list = None,metadata: list = None,chunk_size: int = 1000,chunk_overlap: int = 5,
+                                 file_save_path: str = './text_embeddings.db'):
+        """
+        Function to generate text embeddings
+        Args:
+            text_data_path: list of text files
+            metadata: list of metadata for each text file. Dictionary format
+            chunk_size: size of the chunk
+            chunk_overlap: overlap between chunks
+            file_save_path: path to save the embeddings
+        Returns:
+            None   
+        """
 
+        if self.logger:
+            self.logger.info("Perforing basic checks")
 
+        if self.check_file(file_save_path):
+            return "File already exists"
+
+        if text_data_path is None:
+            return "Please provide text data path"
+
+        assert isinstance(text_data_path, list), "text_data_path should be a list"
+        assert isinstance(metadata, dict), "metadata should be a dictionary"
+        if metadata is not None:
+            assert len(text_data_path) == len(metadata), "Number of text files and metadata should be equal"
+
+        if self.logger:
+            self.logger.info(f"Loading text data from {text_data_path}")
+
+        doc_data = [] 
+        for i in text_data_path:
+            if self.check_file(i):
+                text_loader = TextLoader(i)
+                get_text = text_loader.load()
+                if metadata is not None:
+                    for j in get_text:
+                        j.metadata = metadata[i]
+                        doc_data.append(j)
+            if self.logger:
+                self.logger.info(f"Text data loaded from {i}")
+            else:
+                return f"File {i} not found"
+
+        if self.logger:
+            self.logger.info(f"Splitting text data into chunks of size {chunk_size} with overlap {chunk_overlap}")
+        text_splitter = CharacterTextSplitter(chunk_size=chunk_size,chunk_overlap=chunk_overlap)
+        docs = text_splitter.split(doc_data)
+
+        if self.logger:
+            self.logger.info(f"Generating embeddings for {len(docs)} documents")    
+        self.vector_store.from_documents(docs, self.model,persist_directory=file_save_path)
+        if self.logger:
+            self.logger.info(f"Embeddings generated and saved at {file_save_path}")
+
+    def load_model(self,model: str,model_type: str,**kwargs):
+        if model == 'openai':
+            model = OpenAIEmbeddings(model_type)
+            if self.logger:
+                self.logger.info(f"Loaded model {model_type}")
+            return model
+        else:
+            return "Model not found"
+
+    def load_vectorstore(self,vector_store_type: str,**kwargs):
+        if vector_store_type == 'chroma':
+            vector_store = Chroma()
+            if self.logger:
+                self.logger.info(f"Loaded vector store {vector_store_type}")
+            return vector_store
+        else:
+            return "Vector store not found"
+
+    def load_embeddings(self,embeddings_path: str):
+        """
+        Function to load embeddings
+        Args:
+            embeddings_path: path to the embeddings
+        Returns:
+            embeddings
+        """
+        if self.check_file(embeddings_path):
+            if self.vector_store_type == 'chroma':
+                return Chroma(persist_directory = embeddings_path,embedding_function=self.model)
+        else:
+            return "Embeddings file not found"
+        
+    def load_retriever(self,embeddings_path: str,search_type: str = "similarity_score_threshold" ,search_params: dict = {"k": 3, "score_threshold": 0.9}):
+        """
+        Function to load retriever
+        Args:
+            embeddings_path: path to the embeddings
+        Returns:
+            retriever
+        """
+        if self.check_file(embeddings_path):
+            if self.vector_store_type == 'chroma':
+                db = Chroma(persist_directory = embeddings_path,embedding_function=self.model)
+                self.retriever = db.as_retriever(search_type = search_type,search_kwargs=search_params)
+                if self.logger:
+                    self.logger.info("Retriever loaded")
+                # return self.retriever
+        else:
+            return "Embeddings file not found"
+        
+    def query_embeddings(self,query: str):
+        """
+        Function to query embeddings
+        Args:
+            search_type: type of search
+            query: query to search
+        Returns:
+            results
+        """
+        if self.vector_store_type == 'chroma':
+            return self.retriever.invoke(query)
