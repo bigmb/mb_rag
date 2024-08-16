d477389291896d560c07d6d030dc15ba80a64299
Malav <malav.b93@gmail.com>
2024-08-16 03:37:35 +0100

    updated version.py to 1.0.21

--- mb_rag/rag/embeddings.py
+++ mb_rag/rag/embeddings.py
@@ -5,7 +5,7 @@ from langchain.text_splitter import CharacterTextSplitter
 from langchain_community.document_loaders import TextLoader
 from langchain_community.vectorstores import Chroma
 from langchain_openai import OpenAIEmbeddings
-from rag.utils.extra import load_env_file
+from mb_rag.utils.extra  import load_env_file
 
 load_env_file()
 
