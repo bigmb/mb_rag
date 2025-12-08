b7e005dcbf5cc49f704425d7195e52265640714a
bigmb <malav.b93@gmail.com>
2025-12-08 03:47:37 +0000

    updated version.py to 1.1.92

--- data/scripts_test/run_examples.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-'''
-python3 scripts/agent_segmention_run.py data/scripts_test/sample_tasks.csv 
---num_workers 2 
---use_logger 
---output_dir data/test_output_folders 
---model_name gemini-3-pro-preview'''
